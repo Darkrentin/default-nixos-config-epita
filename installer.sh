65edb6200c24a9add2d39d8ee9fbccfa8e100506
Corentin Del Pozo <corentin.del-pozo@epita.fr>
2025-09-25 15:06:01 +0200

    fix

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/Darkrentin/default-nixos-config-epita.git ~/default-conf/
 
-cp -rT ~/default-conf/ ~/afs/.confs/
+cp -rT ~/default-conf/Config/ ~/afs/.confs/
 
 rm -rf ~/default-conf
